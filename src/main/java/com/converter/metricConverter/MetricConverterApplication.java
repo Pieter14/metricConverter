4665ddea50a9f61f5f2d2190062eea9b53344a26
Pieter van Wyk <pietervw@epcmholdings.com>
2021-11-28 21:51:43 +0200

    Update MetricConverterApplication.java

--- src/main/java/com/converter/metricConverter/MetricConverterApplication.java
+++ src/main/java/com/converter/metricConverter/MetricConverterApplication.java
@@ -2,27 +2,11 @@ package com.converter.metricConverter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
 
 @SpringBootApplication
 public class MetricConverterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MetricConverterApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("PUT", "DELETE",
-						"GET", "POST");
-			}
-		};
 	}
 }
